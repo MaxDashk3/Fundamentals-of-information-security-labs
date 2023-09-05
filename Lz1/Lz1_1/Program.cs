0200824dc8c0d0557fc7422337a4b21f46c561ee
Maxim Dashkovskiy <maxdashk3@gmail.com>
2023-09-05 15:04:35 +0300

    Added first lab

---
+++ Lz1/Lz1_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Lz1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random(1234567890);
+
+            for (int i = 0; i < 10; i++)
+            {
+                int num = random.Next(1, 10);
+                Console.WriteLine(num);
+
+            }
+        }
+    }
+}
---
+++ Lz1/Lz1_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+
+namespace Lz1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rndNumberGenerator = new RNGCryptoServiceProvider();              
+            var randomNumber = new byte[9];
+            rndNumberGenerator.GetBytes(randomNumber);
+            var ConvertedResult = Convert.ToBase64String(randomNumber);
+
+            Console.WriteLine(ConvertedResult);
+
+        }
+    }
+}
