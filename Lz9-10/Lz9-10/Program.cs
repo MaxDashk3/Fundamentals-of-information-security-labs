ab8919a5110ab3d4b23ad3905d63ae09a43e070b
Maxim Dashkovskiy <maxdashk3@gmail.com>
2023-11-30 16:34:55 +0200

    Added lab9-10

---
+++ Lz9-10/Lz9-10/Program.cs
@@ -0,0 +1,57 @@
+﻿using Lz9_10;
+
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+char c = ' ';
+while (c != 'e')
+{
+    Console.WriteLine("Do you want to create a signature (c) or validate an existing one (v)? (press e to exit, press s to save key)");
+    c = Console.ReadKey().KeyChar;
+    Console.WriteLine();
+
+    switch (c)
+    {
+        case 'e':
+            return;
+        case 'v' or 'c':
+            break;
+        case 's':
+            Console.Write("Enter path to save key: ");
+            var savepath = Console.ReadLine().Replace("\"", "");
+            Signature.SavePublic(savepath + "\\PublicKey.xml");
+            Console.WriteLine();
+            continue;
+        default:
+            Console.WriteLine("Error! Unrecognized Command!");
+            continue;
+    }
+
+
+    Console.Write("Enter path to the document: ");
+    var filepath = Console.ReadLine().Replace("\"", "");
+
+    var dir = filepath.Remove(filepath.LastIndexOf("\\") + 1);
+    var filebytes = File.ReadAllBytes(filepath);
+
+    switch (c)
+    {
+        case 'c':
+            var signature = Signature.Generate(filebytes);
+            Console.WriteLine("Signature: " + Convert.ToBase64String(signature) + "\n");
+            File.WriteAllBytes(dir + "Signature.dat", signature);
+            break;
+        case 'v':
+            Console.Write("Enter path to the signature: ");
+            signature = File.ReadAllBytes(Console.ReadLine().Replace("\"", ""));
+
+            Console.Write("Enter path to the public xml key: ");
+            var keypath = Console.ReadLine().Replace("\"", "");
+
+            bool check = Signature.CheckSignature(keypath, filebytes, signature);
+
+            Console.WriteLine(check ? "The signature is correct!" : "The signature is incorrect!");
+            break;
+    }
+}
+
---
+++ Lz9-10/Lz9-10/Signature.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lz9_10
+{
+    internal static class Signature
+    {
+        private readonly static string CspContainerName = "RsaContainer";
+        private static CspParameters cspParams = new CspParameters
+        {
+            KeyContainerName = CspContainerName,
+            Flags = CspProviderFlags.UseMachineKeyStore,
+        };
+
+        public static byte[] Generate(byte[] data)
+        {
+            using (var rsa = new RSACryptoServiceProvider(cspParams))
+            {
+                rsa.PersistKeyInCsp = true;
+                var rsaFormatter = new RSAPKCS1SignatureFormatter(rsa);
+
+                rsaFormatter.SetHashAlgorithm(nameof(SHA512));
+
+                byte[] hashOfData = SHA512.HashData(data);
+
+                return rsaFormatter.CreateSignature(hashOfData);
+            }
+        }
+
+        public static bool CheckSignature(string publicKeyPath, byte[] data, byte[] signature)
+        {
+            using (var rsa = new RSACryptoServiceProvider(cspParams))
+            {
+                rsa.FromXmlString(File.ReadAllText(publicKeyPath));
+                rsa.PersistKeyInCsp = true;
+
+                var rsaDeformatter = new RSAPKCS1SignatureDeformatter(rsa);
+                rsaDeformatter.SetHashAlgorithm(nameof(SHA512));
+
+                byte[] hashOfData = SHA512.HashData(data);
+
+                return rsaDeformatter.VerifySignature(hashOfData, signature);
+            }
+        }
+        public static void SavePublic(string path)
+        {
+            using (var rsa = new RSACryptoServiceProvider(cspParams))
+            {
+                rsa.PersistKeyInCsp = true;
+                File.WriteAllText(path, rsa.ToXmlString(false));
+            }
+        }
+    }
+}
